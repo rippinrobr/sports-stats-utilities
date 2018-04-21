e8de6e87e159afd3479b3df15495cdb24b7d3c0b
Rob Rowe <rrowe@foodlogiq.com>
2018-04-21 15:15:53 -0400

    adding the script

--- scripts/baseball-stats-db/nightly_pull.sh
+++ scripts/baseball-stats-db/nightly_pull.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 RELEASE_VERSION=5
 BASEBALLDATABANK_DIR=~/src/baseballdatabank
+STATS_DB_DIR=~/go/src/github.com/rippinrobr/baseball-stats-db
 SEASON=2017
 
 home_dir=`pwd`
-
 cd $BASEBALLDATABANK_DIR
+echo "In ${BASEBALLDATABANK_DIR}"
 no_update=`git pull origin master | grep up-to-date | wc -l | head -1`
 
 if [ $no_update -eq 1 ]; then
@@ -18,6 +19,6 @@ echo "There was an update, now I need to make a new nightly release"
 commit_hash=`git log -1 | head -1 | cut -d\  -f2`
 echo "Shortened Hash => ${commit_hash:0-7}"
 
+cd $STATS_DB_DIR
+make sqlitedb_bd INC_VERSION=${RELEASE_VERSION} SEASON=${SEASON} 
 cd $home_dir
-
-make INC_VERSION=${RELEASE_VERSION} SEASON=${SEASON} HASH=${commit_hash:0-7}
